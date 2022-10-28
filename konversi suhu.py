7a376097c6e85606acb0feecddbe115d6d6eb892
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-10-28 18:51:40 +0700

    Add files via upload

---
+++ konversi suhu.py
@@ -0,0 +1,18 @@
+print("\nPROGRAM KONVERSI TEMPERATUR\n")
+#celcius
+c = int(input('masukkan suhu dalam celcius :'))
+print()
+print("suhu adalah ", c, "celcius")
+print()
+#reamur
+r = (4/5)*c
+print("suhu dalam reamur adalah ", r, "reamur")
+print()
+#farhenheit
+f = ((9/5)*c+32)
+print("suhu dalam farhenheit adalah ", f, "farhenheit")
+print()
+#kelvin
+k = c+273
+print("suhu dalam kelvin adalah ", k, "kelvin")
+
